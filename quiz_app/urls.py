6b8bac53bac589ff543b33863a4e8e31eb0f23fe
itsRaCl <rajclerk.2004@gmail.com>
2023-01-13 13:52:53 +0530

    fixed gitignore blunder

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -2,4 +2,12 @@ from django.urls import path
 from . import views
 
 app_name = "quiz"
-urlpatterns = [path("", views.home, name="quiz-home")]
+urlpatterns = [
+    path("", views.home, name="quiz-home"),
+    path("view/<int:quiz_id>/", views.view_quiz, name="view-quiz"),
+    path(
+        "take/<int:quiz_id>/<int:question_no>",
+        views.question_view,
+        name="view-question",
+    ),
+]
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Quiz, Question, Choice
 
 # Create your views here.
 def home(request):
     context = {
         "quizes": [
             {
+                "quiz_id": 1,
                 "quiz_title": "Quiz 1",
                 "quiz_master": "Quiz Master 1",
                 "quiz_description": "This is the first quiz by quiz master 1",
@@ -18,16 +20,32 @@ def home(request):
     return render(request, "quiz_app/home.html", context=context)
 
 
-def view_quiz(request):
-    return HttpResponse("<h1> View Quiz </h1>")
+def view_quiz(request, quiz_id):
+    # quiz = Quiz.objects.get(pk=quiz_id)
+    # context = {
+    #     "quiz_id": quiz.id,
+    #     "quiz_title": quiz.quiz_title,
+    #     "quiz_description": quiz.quiz_description,
+    #     "quiz_master": quiz.quiz_master,
+    # }
+    context = {
+        "quiz_id": 1,
+        "quiz_title": "Quiz 1",
+        "quiz_master": "Quiz Master 1",
+        "quiz_description": "This is the first quiz by quiz master 1",
+        "c_marks": 4,
+        "ic_marks": -1,
+        "other_marks": "-",
+    }
+    return render(request, "quiz_app/start_quiz.html", context=context)
 
 
 def start_quiz(request):
     return HttpResponse("<h1> Start Quiz? </h1> ")
 
 
-def question_view(request):
-    return HttpResponse("<h1> Question View </h1>")
+def question_view(request, quiz_id, question_no):
+    return HttpResponse(f"<h1> Question View {question_no}</h1>")
 
 
 def quiz_end(request):
